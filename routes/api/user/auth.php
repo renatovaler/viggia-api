d9b1a2b74037a68d9eaa67e32a87af6050e80b62
Renato <renato.valer@hotmail.com>
2022-05-26 12:11:54 -0400

    mudanças nas rotas e início da implementação de ações do usuário

--- routes/api/user/auth.php
+++ routes/api/user/auth.php
@@ -1,19 +1,41 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Domains\User\Http\Auth\Controllers\UpdateAuthenticatedUserPasswordController;
+use App\Domains\User\Http\Auth\Controllers\GetAuthenticatedUserProfileInformationController;
+use App\Domains\User\Http\Auth\Controllers\UpdateAuthenticatedUserProfileInformationController;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API Routes - User Domain
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| As rotas de API do domínio "User" devem ser registradas aqui.
+|
+| Com exceção das rotas relativas ao usuário logado, as demais devem seguir o seguinte padrão:
+|
+|		Verb		|				URI								|	Action	|		Route Name
+|		GET			|	/photos/{photo}/comments					|	index	|	photos.comments.index
+|		GET			|	/photos/{photo}/comments/create				|	create	|	photos.comments.create
+|		POST		|	/photos/{photo}/comments					|	store	|	photos.comments.store
+|		GET			|	/photos/{photo}/comments/{comment}			|	show	|	photos.comments.show
+|		GET			|	/photos/{photo}/comments/{comment}/edit		|	edit	|	photos.comments.edit
+|		PUT/PATCH	|	/photos/{photo}/comments/{comment}			|	update	|	photos.comments.update
+|		DELETE		|	/photos/{photo}/comments/{comment}			|	destroy	|	photos.comments.destroy
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/profile', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+		// Get authenticated user profile information
+		Route::get('my-profile', [GetAuthenticatedUserProfileInformationController::class, '__invoke'])
+		->name('api.users.auth.myprofile.show');
+
+		// Update authenticated user profile information
+		Route::put('my-profile', [UpdateAuthenticatedUserProfileInformationController::class, '__invoke'])
+		->name('api.users.auth.myprofile.update');
+
+		// Update authenticated user password
+		Route::put('my-password', [UpdateAuthenticatedUserPasswordController::class, '__invoke'])
+		->name('api.users.auth.mypassword.update');
 });
