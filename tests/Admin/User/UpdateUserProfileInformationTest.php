b03d64faf2f186f3d5d36bc5b9f7b29884a16f5f
renato.valer@hotmail.com <renato.valer@hotmail.com>
2022-07-29 11:41:14 +0000

    novos testes de Admin/User

--- tests/Admin/User/UpdateUserProfileInformationTest.php
+++ tests/Admin/User/UpdateUserProfileInformationTest.php
@@ -2,88 +2,178 @@
 
 namespace Tests\Admin\User;
 
+use App\User\Models\User;
+
 use Tests\TestCase;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UpdateUserProfileInformationTest extends TestCase
 {
-    use RefreshDatabase;
     use WithFaker;
+    use RefreshDatabase;
 
-    public function _test_update_user_profile_with_not_authenticated_user()
+	/*
+	*	Teste de update de um user com usuário não logado
+	*	Usuário logado: NÃO
+	*	Usuário autorizado (nível de acesso): PREJUDICADO
+	*	Parâmetros de request válidos: PREJUDICADO
+	*/
+    public function test_update_user_profile_with_not_authenticated_user()
     {
+		// Verifica se o usuário não está logado
+		$this->assertGuest();
+
+        // Cria um usuário qualquer
+		$targetUser = $this->createCommonUser();
+
 		// Faz a requisição para atualizar o registro sem informar usuário logado
-		$response = $this->putUpdateUser( self::NOT_AUTHENTICATED );
+		$response = $this->putJson('/admin/users/' .$targetUser->id. '/profile', []);
 
-		// Verifica se o usuário está logado
-		$this->assertAuthenticated();
-
+		// Verifica se os dados continuam igual, sem atualizar
+		$this->assertDatabaseHas('users', [
+			'id' => $targetUser->id,
+			'name' => $targetUser->name,
+			'email' => $targetUser->email,
+			'email_verified_at' => $targetUser->email_verified_at
+		]);
+		
 		// Verifica se a resposta foi do tipo "não autorizado" (401)
-        $response->assertUnauthorized();
+		$response->assertUnauthorized();
     }
 
-    public function _test_update_user_profile_with_common_user()
+	/*
+	*	Teste de update de um user com usuário logado, mas não autorizado
+	*	Usuário logado: SIM
+	*	Usuário autorizado (nível de acesso): NÃO
+	*	Parâmetros de request válidos: PREJUDICADO
+	*/
+    public function test_update_user_profile_with_authenticated_but_unauthorized_user()
     {
-		// Faz a requisição para atualizar o registro com usuário comum
-		$response = $this->putUpdateUser( self::AUTHENTICATED, self::COMMON_USER );
-
-		// Verifica se o usuário está logado
+        // Cria um usuário comum [logged]
+        $loggedUser = $this->createCommonUser();
+        
+		// Faz login
+		Auth::loginUsingId($loggedUser->id);
+        
+		// Verifica se o usuário "loggedUser" está logado
 		$this->assertAuthenticated();
 
-		// Verifica se a resposta foi do tipo "não autorizado" (401)
-        $response->assertUnauthorized();
+        // Cria um usuário qualquer [target]
+        $targetUser = $this->createCommonUser();
+
+		// Array com parâmetros de atualização
+		$data = [
+			'id' => $targetUser->id,
+			'name' => 'teste',
+			'email' => $targetUser->email
+		];
+		
+		// Faz a requisição para atualizar o registro informando usuário logado
+		$response = $this->actingAs($loggedUser)->putJson('/admin/users/' .$targetUser->id. '/profile', $data);
+
+		// Verifica se os dados continuam igual, sem atualizar
+		$this->assertDatabaseHas('users', [
+			'id' => $targetUser->id,
+			'name' => $targetUser->name,
+			'email' => $targetUser->email,
+			'email_verified_at' => $targetUser->email_verified_at
+		]);
+
+		// Verifica se a resposta foi do tipo "proibido" (403)
+		// Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
+		$response->assertForbidden();
     }
 
-    public function _test_update_user_profile_with_admin_user()
+	/*
+	*	Teste de update de um user com usuário logado, autorizado, mas sim parâmetros inválidos
+	*	Usuário logado: SIM
+	*	Usuário autorizado (nível de acesso): SIM
+	*	Parâmetros de request válidos: NÃO
+	*/
+    public function test_update_user_profile_with_authenticated_and_authorized_user_but_invalid_params()
     {
-		// Faz a requisição para atualizar o registro com usuário comum
-		$response = $this->putUpdateUser( self::AUTHENTICATED, self::ADMIN_USER );
-
-		// Verifica se o usuário está logado
+        // Cria um usuário admin [logged]
+        $loggedUser = $this->createAdminUser();
+        
+		// Faz login
+		Auth::loginUsingId($loggedUser->id);
+        
+		// Verifica se o usuário "loggedUser" está logado
 		$this->assertAuthenticated();
+
+        // Cria um usuário qualquer [target]
+        $targetUser = $this->createCommonUser();
+		
+		// Faz a requisição para atualizar o registro informando usuário logado e parâmetros de atualização inválidos
+		$response = $this->actingAs($loggedUser)->putJson('/admin/users/' .$targetUser->id. '/profile', []);
+
+		// Verifica se os dados continuam igual, sem atualizar
+		$this->assertDatabaseHas('users', [
+			'id' => $targetUser->id,
+			'name' => $targetUser->name,
+			'email' => $targetUser->email,
+		]);
+
+		// Verifica se está correta a estrutura do JSON de resposta de erro de validação
+        $response->assertJsonStructure([
+			'message',
+            'errors' => []
+		]);
+		
+		// Verifica se o código de resposta HTTP está correto (422)
+		$response->assertUnprocessable();
+    }
+
+	/*
+	*	Teste de update de um user com usuário logado, autorizado e com parâmetros válidos
+	*	Usuário logado: SIM
+	*	Usuário autorizado (nível de acesso): SIM
+	*	Parâmetros de request válidos: SIM
+	*/
+    public function test_update_user_profile_with_authenticated_and_authorized_user_and_valid_params()
+    {
+        // Cria um usuário admin [logged]
+        $loggedUser = $this->createAdminUser();
+        
+		// Faz login
+		Auth::loginUsingId($loggedUser->id);
+        
+		// Verifica se o usuário "loggedUser" está logado
+		$this->assertAuthenticated();
+
+        // Cria um usuário qualquer [target]
+        $targetUser = $this->createCommonUser();
+
+		// Array com parâmetros de atualização
+		$data = [
+			'id' => $targetUser->id,
+			'name' => 'teste',
+			'email' => 'teste@email.com'
+		];
+		
+		// Faz a requisição para atualizar o registro informando usuário logado
+		$response = $this->actingAs($loggedUser)->putJson('/admin/users/' .$targetUser->id. '/profile', $data);
+
+		// Verifica se os dados foram atualizados
+		$this->assertDatabaseHas('users', [
+			'id' => $targetUser->id,
+			'name' => 'teste',
+			'email' => 'teste@email.com'
+		]);
 
 		// Verifica se está correta a estrutura do JSON de resposta
         $response->assertJsonStructure([
 			'data' => [
-				[
-					'id',
-					'name',
-					'email',
-					'email_verified_at'
-				]
+				'id',
+				'name',
+				'email',
+				'email_verified_at'
 			]
 		]);
+		
 		// Verifica se o código de resposta HTTP está correto (200)
 		$response->assertOk();
     }
-
-	/*
-	* Create Vehicle Localization for tests
-	* @return result of post http request
-	*/
-	public function putUpdateUser(bool $authenticated = false, bool $commonUser = true)
-	{
-        // Cria um usuário qualquer
-		$targetUser = $this->createCommonUser();
-
-		// Monta o array com os dados de atualização
-		$data = [
-			'id' => $targetUser->id,
-			'name' => $this->faker->name(),
-			'email' => $targetUser->email
-		];
-
-		// Verifica se a requisição deve ser com usuário logado ou não
-		if(true === $authenticated) {
-
-			// Verifica se o usuário deve ser comum ou admin/super admin
-			$loggedUser = (true === $commonUser ? $this->createCommonUser() : $this->createAdminUser());
-
-			// Retorna a resposta da requisição feita com usuário logado
-			return $this->actingAs($loggedUser)->putJson('/admin/users/' .$targetUser->id. '/profile', $data);
-		}
-		// Retorna a resposta da requisição feita sem usuário logado
-		return $this->putJson('/admin/users/' .$targetUser->id. '/profile', $data);
-	}
 }
