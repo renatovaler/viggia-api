406ae97059f406bb4f4eb143943193e570822228
renato.valer@hotmail.com <renato.valer@hotmail.com>
2022-08-08 19:59:54 +0000

    novos testes de convite de membros

--- tests/Company/CompanyMember/AcceptInviteWithNonExistingUserTest.php
+++ tests/Company/CompanyMember/AcceptInviteWithNonExistingUserTest.php
@@ -41,7 +41,7 @@ class AcceptInviteWithNonExistingUserTest extends TestCase
 	 * Parâmetros válidos: SIM
 	 * Convite expirado: NÃO
 	*/
-	public function test_accept_invite_to_company_with_non_existing_user()
+	public function test_accept_invite_to_company_with_non_existing_user_valid_params_and_valid_invite()
 	{
 		// Cria um usuário Owner da empresa
 		$ownerUser = $this->createCommonUser();
@@ -129,5 +129,196 @@ class AcceptInviteWithNonExistingUserTest extends TestCase
 
 		// Verifica se a requisição foi um sucesso com retorno "NoContent"
 		$response->assertNoContent();
+	}
+	
+	/**
+	 * Teste - aceitar convite com usuário não cadastrado, convite válido, mas parâmetros de request inválidos
+	 * Usuário já cadastrado: NÃO
+	 * Autorizado: SIM
+	 * Parâmetros válidos: NÃO
+	 * Convite expirado: NÃO
+	*/
+	public function test_accept_invite_to_company_with_non_existing_user_valid_invite_but_invalid_params()
+	{
+		// Cria um usuário Owner da empresa
+		$ownerUser = $this->createCommonUser();
+
+		// Faz login
+		Auth::loginUsingId($ownerUser->id);
+
+		// Verifica se a coluna "current_company_id" do usuário logado = null
+		$this->assertDatabaseHas('users', [
+			'id' => Auth::user()->id,
+			'current_company_id' => null
+		]);
+		
+		// Verifica se o usuário está logado
+		$this->assertAuthenticated();
+
+		// Cria a empresa
+		$company = $this->createCompany($ownerUser->id);
+
+		/*
+		* Tenta setar a empresa criada como "current company" do usuário memberUser
+		* -> Se o usuário for o owner, current_company_id será atualizada
+		* -> Se o usuário NÃO for o owner, current_company_id continuará NULL
+		*/
+		Auth::user()->switchCompany($company);
+
+		// Verifica se a coluna "current_company_id" do usuário logado foi atualizada
+		$this->assertDatabaseHas('users', [
+			'id' => Auth::user()->id,
+			'current_company_id' => $company->id
+		]);
+
+		// Faz logout do Owner
+		Auth::logout();
+
+		// Verifica se o Owner foi mesmo deslogado
+        $this->assertGuest();
+
+		// Gera um e-mail a para ser convidado como membro da equipe
+
+		$email = $this->generateUniqueEmail();
+
+		// Confirma que esse usuário novo não existe
+		$this->assertDatabaseMissing('users', [
+			'email' => $email
+		]);
+
+		// Cria o convite
+		$invitation = CompanyInvitationModel::create([
+			'company_id' => $company->id,
+			'email' => $email,
+			'roles' => [
+				['id' => $systemRole = ( (new Role())->where('name', 'common_user')->first() )->id],
+				['id' => $companyRole = ( (new Role())->where('name', 'company_member')->first() )->id]
+			],
+			'token' => (string) Str::orderedUuid(),
+			'expires_in' => Carbon::now()->addHours(48)
+		]);
+
+		// Faz o request para aceitar o convite
+        $response = $this->postJson('/companies/company-invitations/accept', []);
+
+		// Confirma que o usuário NÃO foi adicionado à equipe
+		$this->assertFalse(
+			$company->fresh()->hasOnlyCompanyMemberWithEmail($invitation->email)
+		);
+
+		// Verifica se está correta a estrutura do JSON de resposta
+        $response->assertJsonStructure([
+			'message',
+			'errors' => []
+		]);
+
+		// Verifica se o código de resposta HTTP está correto (422)
+		$response->assertUnprocessable();
+
+		// Confirma que o convite NÃO foi deletado
+		$this->assertDatabaseHas('company_invitations', [
+			'id' => $invitation->id,
+			'token' => $invitation->token
+		]);
+	}
+
+	/**
+	 * Teste - aceitar convite com usuário não cadastrado, mas com convite expirado
+	 * Usuário já cadastrado: NÃO
+	 * Autorizado: SIM
+	 * Parâmetros válidos: SIM
+	 * Convite expirado: SIM
+	*/
+	public function test_accept_invite_to_company_with_existing_user_but_expired_invite()
+	{
+		// Cria um usuário Owner da empresa
+		$ownerUser = $this->createCommonUser();
+
+		// Faz login
+		Auth::loginUsingId($ownerUser->id);
+
+		// Verifica se a coluna "current_company_id" do usuário logado = null
+		$this->assertDatabaseHas('users', [
+			'id' => Auth::user()->id,
+			'current_company_id' => null
+		]);
+		
+		// Verifica se o usuário está logado
+		$this->assertAuthenticated();
+
+		// Cria a empresa
+		$company = $this->createCompany($ownerUser->id);
+
+		/*
+		* Tenta setar a empresa criada como "current company" do usuário memberUser
+		* -> Se o usuário for o owner, current_company_id será atualizada
+		* -> Se o usuário NÃO for o owner, current_company_id continuará NULL
+		*/
+		Auth::user()->switchCompany($company);
+
+		// Verifica se a coluna "current_company_id" do usuário logado foi atualizada
+		$this->assertDatabaseHas('users', [
+			'id' => Auth::user()->id,
+			'current_company_id' => $company->id
+		]);
+
+		// Faz logout do Owner
+		Auth::logout();
+
+		// Verifica se o Owner foi mesmo deslogado
+        $this->assertGuest();
+
+		// Gera um e-mail a para ser convidado como membro da equipe
+
+		$email = $this->generateUniqueEmail();
+
+		// Confirma que esse usuário novo não existe
+		$this->assertDatabaseMissing('users', [
+			'email' => $email
+		]);
+
+		// Cria o convite com token já expirado
+		$invitation = CompanyInvitationModel::create([
+			'company_id' => $company->id,
+			'email' => $email,
+			'roles' => [
+				['id' => $systemRole = ( (new Role())->where('name', 'common_user')->first() )->id],
+				['id' => $companyRole = ( (new Role())->where('name', 'company_member')->first() )->id]
+			],
+			'token' => (string) Str::orderedUuid(),
+			'expires_in' => Carbon::now()->subHours(48) // Token expirado
+		]);
+
+		// Monta o array com os parâmetros do request
+		$data = [
+			'token' => $invitation->token,
+			'email' => $invitation->email,
+			'name' => $this->faker->name(),
+            'password' => 'password',
+            'password_confirmation' => 'password',
+		];
+
+		// Faz o request para aceitar o convite
+        $response = $this->postJson('/companies/company-invitations/accept', $data);
+
+		// Confirma que o usuário NÃO foi adicionado à equipe
+		$this->assertFalse(
+			$company->fresh()->hasOnlyCompanyMemberWithEmail($invitation->email)
+		);
+
+		// Verifica se está correta a estrutura do JSON de resposta
+        $response->assertJsonStructure([
+			'message',
+			'errors' => []
+		]);
+
+		// Verifica se o código de resposta HTTP está correto (422)
+		$response->assertUnprocessable();
+
+		// Confirma que o convite NÃO foi deletado
+		$this->assertDatabaseHas('company_invitations', [
+			'id' => $invitation->id,
+			'token' => $invitation->token
+		]);
 	}
 }
